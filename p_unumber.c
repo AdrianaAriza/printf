6ad45c3f06e053a525ea65b3aae30611709e07df
AdrianaAriza <1296@holbertonschool.com>
2019-10-29 17:51:48 -0500

    [Fix]: Betty

--- p_unumber.c
+++ p_unumber.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * print_unumber - Print number
- * @n: input number
+ * p_unumber - Print number
+ * @arguments: input number
  * @buffer: memory field to host the printf output
  * @i_b: buffer position
  *
@@ -10,9 +10,9 @@
  */
 int p_unumber(va_list arguments, char *buffer, int i_b)
 {
-	long int b = 1, i = 0, j, n = 0,m = 0, aux = 4294967296;
+	long int b = 1, i = 0, j, n = 0, m = 0, aux = 4294967296;
+
 	m = va_arg(arguments, int);
-	
 	if (m < 0)
 		n = aux + m;
 	while (n / b > 9)
@@ -23,7 +23,7 @@ int p_unumber(va_list arguments, char *buffer, int i_b)
 	for (j = 0; j <= i; j++, i_b++)
 	{
 		buffer[i_b] = ((n / b) + '0');
-	       	n = n % b;
+		n = n % b;
 		b = b / 10;
 	}
 	return (i_b);
