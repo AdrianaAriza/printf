45acbeef351987175c138f6e0413b0c57a1678da
AdrianaAriza <1296@holbertonschool.com>
2019-10-29 20:24:24 -0500

    [Fix] If NULL

--- p_point.c
+++ p_point.c
@@ -11,10 +11,31 @@
 
 int p_point(va_list arguments, char *buffer, int i_b)
 {
+	unsigned int decimal, remainder, i, j = 0;
+	char hexadecimal[100];
+	char *p = "(nil)";
+
+	decimal = va_arg(arguments, unsigned int);
+	if (decimal == 0)
+	{
+		for (; p[j]; j++, i_b++)
+			buffer[i_b] = p[j];
+		return (i_b);
+	}
+	while (decimal)
+	{
+		remainder = decimal % 16;
+		if (remainder < 10)
+			hexadecimal[j++] = 48 + remainder;
+		else
+			hexadecimal[j++] = 87 + remainder;
+		decimal = decimal / 16;
+	}
 	buffer[i_b] = 48;
 	i_b++;
 	buffer[i_b] = 120;
 	i_b++;
-	i_b = p_hexa(arguments, buffer, i_b);
+	for (i = j; i > 0; i--, i_b++)
+		buffer[i_b] = hexadecimal[i - 1];
 	return (i_b);
 }
